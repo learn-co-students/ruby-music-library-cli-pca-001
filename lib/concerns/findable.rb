9bc47eefa4df0b5928565713908eefd6b7eaadba
Madison McCardle <madison.mccardle@powerhrg.com>
2020-05-14 13:55:39 -0400

    lets hope im prepared

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,12 @@
+module Concerns::Findable
+    def find_by_name(name)
+        self.all.find do |itself|
+            itself.name == name
+        end
+    end
+
+    def find_or_create_by_name(name)
+        find_by_name(name) || create(name)
+    end
+
+  end

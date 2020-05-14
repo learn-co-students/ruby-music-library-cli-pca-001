0aae9065aa478fb6c1c2f87c8b1d9f935c2ed55a
Idan <idan.zonshein@powerhrg.com>
2020-05-14 11:38:01 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
 
-gem 'rspec'
-gem 'pry'
+gem "pry"
 gem 'require_all',  '~> 1.0'
+gem "rspec"
---
+++ lib/Artist.rb
@@ -0,0 +1,42 @@
+class Artist
+  extend Concerns::Findable
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = new(name)
+    artist.save
+    artist
+  end
+
+  def add_song(song)
+    song.artist = self if song.artist == nil
+    @songs << song if @songs.include?(song) == false
+  end
+
+  def genres
+    songs.map(&:genre).uniq
+  end
+
+
+
+end
